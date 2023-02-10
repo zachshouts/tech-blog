cbe84dea682887c439d58ddf4ebc5ba2fd68b0ff
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-02-09 18:48:40 -0600

    Added table relationships to models/index.js

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const { User, Post, Comment } = require('../models');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
