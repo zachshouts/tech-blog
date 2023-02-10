cf12293a9a8c6897d884b81eaf85c1ec8ea79e4c
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-02-09 18:44:34 -0600

    Added models for the db

---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "User",
+                key: "id",
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "Post",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "Comment",
+    }
+);
